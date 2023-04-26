277ca0b16c2522927b48580683cdd5cbcb5e50b5
Przemysław Frąckowiak-Szymański <7953131+dehydrogenaza@users.noreply.github.com>
2023-04-26 19:36:59 +0200

    basic setup

--- random-names-api/src/main/java/com/dehydrogenaza/randomnamesapi/RandomNamesApiApplication.java
+++ random-names-api/src/main/java/com/dehydrogenaza/randomnamesapi/RandomNamesApiApplication.java
@@ -2,8 +2,10 @@ package com.dehydrogenaza.randomnamesapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class RandomNamesApiApplication {
 
     public static void main(String[] args) {
